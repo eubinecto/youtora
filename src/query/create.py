6ad97dabe8435364df466e24ad49a6e745b180b6
Eu-Bin KIM <tlrndk123@gmail.com>
2020-08-08 08:21:37 +0900

    new class: IdxQuery
    the index queries have been changed accordingly.

--- src/query/create.py
+++ src/query/create.py
@@ -2,59 +2,64 @@
 # for creating youtora
 from src.es.restAPIs.idxAPIs.idxManagement import CreateIdxAPI
 
-YOUTORA_TRACKS_IDX_MAPPINGS = {
-  "mappings": {
-    "properties": {
-      "start": {
-        "type": "double"
-      },
-      "duration": {
-        "type": "double"
-      },
-      "content": {
-        "type": "text"
-      },
-      "caption": {
+
+class IdxQuery:
+    YOUTORA_TRACKS_IDX_NAME = "youtora_tracks"
+    YOUTORA_TRACKS_IDX_MAPPINGS = {
+      "mappings": {
         "properties": {
-          "id": {
-            # keyword data type: only searchable by the exact value
-            # https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html
-            "type": "keyword"
+          "start": {
+            "type": "double"
           },
-          "is_auto": {
-            "type" : "boolean"
+          "duration": {
+            "type": "double"
           },
-          "lang_code": {
-            "type": "keyword"
+          "content": {
+            "type": "text"
           },
-          "video": {
+          "caption": {
             "properties": {
               "id": {
+                # keyword data type: only searchable by the exact value
+                # https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html
                 "type": "keyword"
               },
-              "views": {
-                "type": "rank_feature"
+              "is_auto": {
+                "type" : "boolean"
               },
-              "likes": {
-                "type": "rank_feature"
+              "lang_code": {
+                "type": "keyword"
               },
-              "dislikes": {
-                "type": "rank_feature",
-                "positive_score_impact": False
-              },
-              "l_to_d": {
-                "type": "rank_feature"
-              },
-              "publish_date_int": {
-                "type": "rank_feature"
-              },
-              "channel": {
+              "video": {
                 "properties": {
                   "id": {
                     "type": "keyword"
                   },
-                  "subs": {
+                  "views": {
                     "type": "rank_feature"
+                  },
+                  "likes": {
+                    "type": "rank_feature"
+                  },
+                  "dislikes": {
+                    "type": "rank_feature",
+                    "positive_score_impact": False
+                  },
+                  "like_ratio": {
+                    "type": "rank_feature"
+                  },
+                  "publish_date_int": {
+                    "type": "rank_feature"
+                  },
+                  "channel": {
+                    "properties": {
+                      "id": {
+                        "type": "keyword"
+                      },
+                      "subs": {
+                        "type": "rank_feature"
+                      }
+                    }
                   }
                 }
               }
@@ -63,63 +68,58 @@ YOUTORA_TRACKS_IDX_MAPPINGS = {
         }
       }
     }
-  }
-}
 
-
-YOUTORA_COLL_IDX_MAPPINGS = {
-  "mappings": {
-    "properties": {
-      "doc_type": {
-        "type": "keyword"
-      },
-      "url": {
-        "type": "keyword"
-      },
-      "title": {
-        "type": "text"
-      },
-      "lang_code": {
-        "type": "keyword"
-      },
-      "is_auto": {
-        "type": "boolean"
-      },
-      "views": {
-        "type": "integer"
-      },
-      "dislikes": {
-        "type": "integer"
-      },
-      "l_to_d": {
-        "type": "integer"
-      },
-      "publish_date": {
-        "type": "date"
-      },
-      "publish_date_int": {
-        "type": "integer"
+    YOUTORA_COLL_IDX_NAME = "youtora_coll"
+    YOUTORA_COLL_IDX_MAPPINGS = {
+      "mappings": {
+        "properties": {
+          "doc_type": {
+            "type": "keyword"
+          },
+          "url": {
+            "type": "keyword"
+          },
+          "title": {
+            "type": "text"
+          },
+          "lang_code": {
+            "type": "keyword"
+          },
+          "is_auto": {
+            "type": "boolean"
+          },
+          "views": {
+            "type": "integer"
+          },
+          "dislikes": {
+            "type": "integer"
+          },
+          "like_ratio": {
+            "type": "integer"
+          },
+          "publish_date": {
+            "type": "date"
+          },
+          "publish_date_int": {
+            "type": "integer"
+          }
+        }
       }
     }
-  }
-}
 
+    @classmethod
+    def create_youtora_idx(cls):
+        """
+        creates the youtora index with the given configurations
+        """
+        # create youtora_tracks
+        # de-normalisation of the fields.
+        CreateIdxAPI.create_idx(index=cls.YOUTORA_TRACKS_IDX_NAME,
+                                mappings=cls.YOUTORA_TRACKS_IDX_MAPPINGS)
 
-def create_youtora_idx():
-    """
-    creates the youtora index with the given configurations
-    """
-    global YOUTORA_TRACKS_IDX_MAPPINGS
-    global YOUTORA_COLL_IDX_MAPPINGS
-
-    # create youtora_tracks
-    # de-normalisation of the fields.
-    CreateIdxAPI.create_idx(index="youtora_tracks",
-                            mappings=YOUTORA_TRACKS_IDX_MAPPINGS)
-
-    # create youtora_coll
-    CreateIdxAPI.create_idx(index="youtora_coll",
-                            mappings=YOUTORA_COLL_IDX_MAPPINGS)
+        # create youtora_coll
+        CreateIdxAPI.create_idx(index=cls.YOUTORA_COLL_IDX_NAME,
+                                mappings=cls.YOUTORA_COLL_IDX_MAPPINGS)
 
 
 
