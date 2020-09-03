498b6be735fdc55bd48796db442ae8d303d02f1c
DicoTiar <mw18074@bristol.ac.uk>
2020-09-03 14:37:00 +0900

    vuex store added #82

--- fe/youtora_fe/src/main.js
+++ fe/youtora_fe/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuex from 'vuex'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
