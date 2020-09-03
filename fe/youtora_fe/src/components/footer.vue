054a986450ecfaabd0c720a071af7428c4443189
DicoTiar <mw18074@bristol.ac.uk>
2020-09-03 21:37:28 +0900

    pagination 진행중 #88

---
+++ fe/youtora_fe/src/components/footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="Foot" class="overflow-auto">
+        <b-pagination
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="this.$store.state.search.perPage"
+                align="center"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Foot',
+        data() {
+            return {
+                rows: 60,
+                currentPage: 1,
+            }
+        },
+        methods: {
+
+        },
+        watch: {
+            // currentPage: function(val) {
+            //     //do something when the data changes.
+            //     if (val) {
+            //         this.$store.commit('SET_CURRENT_PAGE', val)
+            //         console.log(this.$store.state.currentPage)
+            //     }
+            // }
+        }
+    }
+</script>
+
+<style>
+
+</style>
