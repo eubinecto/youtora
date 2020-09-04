98ec9d64a7318af819a129621dc1c86351959cd9
Eu-Bin KIM <tlrndk123@gmail.com>
2020-09-04 19:35:52 +0900

    [#95] temp_dloaders.py: just download the image and store it in a path.

--- be/src/youtora/temp_dloaders.py
+++ be/src/youtora/temp_dloaders.py
@@ -14,30 +14,29 @@ class FrameDownloader:
     # as for just text detection, this will suffice
     SETTINGS_DICT = {
         'format_code': '135',  # 480P
-        'frame_res': (480, 854)  # height , width\
+        'frame_format': 'jpeg'
     }
-    # on mac & linux
-    FFMPEG_BIN = "ffmpeg"
-
-    DATA_URL_FORM = 'data:image/png;base64,{}'
 
     @classmethod
-    def dl_frame_bytes(cls, vid_url, timestamp) -> bytes:
+    def dl_frame_jpeg(cls,
+                      vid_url,
+                      timestamp,
+                      out_path) -> None:
         """
+        :param out_path:
         :param vid_url: the video from which to download the frames
         :param timestamp: the timestamp at which to capture the frame
-        :return: a numpy array of the image
         """
         # credit: http://zulko.github.io/blog/2013/09/27/read-and-write-video-frames-in-python-using-ffmpeg/
         cmd = "ffmpeg -ss '{timestamp}'" \
               " -i $(youtube-dl -f {format_code} --get-url '{vid_url}')" \
-              " -f image2pipe" \
-              " -pix_fmt rgb24" \
               " -vframes 1" \
               " -q:v 2" \
-              " -vcodec rawvideo" \
-              " -" \
-            .format(timestamp=timestamp, format_code=cls.SETTINGS_DICT['format_code'], vid_url=vid_url)
+              " {out_path}" \
+            .format(timestamp=timestamp,
+                    format_code=cls.SETTINGS_DICT['format_code'],
+                    vid_url=vid_url,
+                    out_path=out_path)
 
         # build the subprocess
         proc = subprocess.Popen(
@@ -46,26 +45,5 @@ class FrameDownloader:
             stdout=subprocess.PIPE,
             bufsize=10**8  # should be bigger than the size of the frame
         )
-        # get the raw byte image and terminate the process
-        img_bytes = proc.communicate()[0]
-        proc.terminate()
-        return img_bytes
-
-    @classmethod
-    def _img_bytes_to_np(cls,
-                         img_bytes: List[bytes],
-                         height: int,
-                         width: int) -> np.array:
-        # decode the bytes to integers
-        image = np.frombuffer(img_bytes, 'uint8')  # decode the bytes
-        # reshape with the given resolution.
-        image = image.reshape((height, width, 3))
-        return image
-
-    @classmethod
-    def _img_bytes_to_data_url(cls,
-                               img_bytes: List[bytes]):
-        encoded = base64.b64encode(img_bytes).decode()
-        return cls.DATA_URL_FORM.format(encoded)
-
+        proc.communicate()
 
