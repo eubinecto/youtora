32a5a632497dcf3243668b674584ee6905303260
DicoTiar <mw18074@bristol.ac.uk>
2020-09-04 16:51:57 +0900

    검색 언어 vuex store에 추가 #88

--- fe/youtora_fe/src/components/navBar.vue
+++ fe/youtora_fe/src/components/navBar.vue
@@ -19,12 +19,15 @@
             </b-form-group>
 
             <b-form-group id="input-group-3" label="Target Language:" label-for="input-3">
-                <b-form-select
-                        id="input-3"
-                        v-model="form.lang"
-                        :options="langs"
-                        required
-                ></b-form-select>
+                <b-form-radio-group
+                        id="langId"
+                        v-model="lang"
+                        :options="optLang"
+                        class="mb-3"
+                        value-field="item"
+                        text-field="name"
+                        name="langId"
+                ></b-form-radio-group>
             </b-form-group>
 
             <b-form-group label="perPageRadio">
@@ -40,7 +43,6 @@
             </b-form-group>
 
             <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
         </b-form>
 
     </div>
@@ -62,9 +64,14 @@
                 ],
                 form: {
                     query: '',
-                    lang: null,
                 },
-                langs: [{ text: 'Select One', value: null }, 'ko', 'en', 'fr', 'jp'],
+                lang:'en',
+                optLang:[
+                    {item: 'ko', 'name': 'Korean'},
+                    {item: 'en', 'name': 'English'},
+                    {item: 'fr', 'name': 'French'},
+                    {item: 'jp', 'name': 'Japanese'},
+                ],
                 show: true
             }
         },
@@ -73,16 +80,21 @@
                     //do something when the data changes.
                     if (val) {
                         this.$store.commit('SET_PER_PAGE', val)
-
                     }
+                },
+            lang: function(val) {
+                //do something when the data changes.
+                if (val) {
+                    this.$store.commit('SET_SEARCH_LANGUAGE', val)
                 }
+            },
         },
         methods: {
             onSubmit(evt) {
                 evt.preventDefault()
 
                 this.$store.commit('SET_PER_PAGE', this.perPage)
-                this.$store.commit('SET_SEARCH_LANGUAGE', this.form.lang)
+                this.$store.commit('SET_SEARCH_LANGUAGE', this.lang)
                 this.$store.commit('SET_SEARCH_QUERY', this.form.query)
 
                 this.$store.dispatch('SEARCH_VIDEOS')
@@ -90,10 +102,10 @@
             onReset(evt) {
                 evt.preventDefault()
                 // Reset our form values
-                this.form.email = ''
-                this.form.name = ''
-                this.form.food = null
-                this.form.checked = []
+                this.perPage = 5
+                this.form.query = ''
+                this.lang = 'en'
+
                 // Trick to reset/clear native browser form validation state
                 this.show = false
                 this.$nextTick(() => {
