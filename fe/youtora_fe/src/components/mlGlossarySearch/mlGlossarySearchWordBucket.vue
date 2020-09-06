458c07d64c52faca8ef4ecd015df95b908a8fd9d
DicoTiar <mw18074@bristol.ac.uk>
2020-09-06 12:09:09 +0900

    mlGlossary alphabetically grouped #99

--- fe/youtora_fe/src/components/mlGlossarySearch/mlGlossarySearchWordBucket.vue
+++ fe/youtora_fe/src/components/mlGlossarySearch/mlGlossarySearchWordBucket.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="mlWordBucket">
-        <b-card-group>
-            <div v-for="item in glossaries" :key="item._id">
-                <b-card>
-                    <b-button @click="setModal(item)">{{ item.word }}</b-button>
-                </b-card>
-            </div>
-            <b-modal v-model="modalShow" :title="this.modalWord">
-                description: <br/>
-                {{ this.modalDesc }}
-            </b-modal>
+
+        <b-card-group v-for="alphabet in Object.keys(alphabetGlossaries)" :key="alphabet">
+            <b-card align="left">
+                <h3>{{ alphabet.toUpperCase() }}</h3>
+                <b-card-group>
+                    <div v-for="item in alphabetGlossaries[alphabet]" :key="item._id">
+                        <b-card>
+                            <b-button @click="setModal(item)">{{ item.word }}</b-button>
+                        </b-card>
+                    </div>
+                </b-card-group>
+            </b-card>
         </b-card-group>
+        <b-modal v-model="modalShow" :title="this.modalWord">
+            description: <br/>
+            {{ this.modalDesc }}
+        </b-modal>
     </div>
 </template>
 
@@ -25,6 +31,45 @@
             }
         },
         methods: {
+            getAlphabetGlossary: function() {
+                const glossaries = this.glossaries
+                var glossaryDict = {
+                    'a': [],
+                    'b': [],
+                    'c': [],
+                    'd': [],
+                    'e': [],
+                    'f': [],
+                    'g': [],
+                    'h': [],
+                    'i': [],
+                    'j': [],
+                    'k': [],
+                    'l': [],
+                    'm': [],
+                    'n': [],
+                    'o': [],
+                    'p': [],
+                    'q': [],
+                    'r': [],
+                    's': [],
+                    't': [],
+                    'u': [],
+                    'v': [],
+                    'w': [],
+                    'x': [],
+                    'y': [],
+                    'z': []
+
+                }
+                for (let i = 0; i < glossaries.length; i++) {
+                    var curWord = glossaries[i].word.replace(/ /g, "")
+                    var firstChar = curWord.charAt(0).toLowerCase()
+                    glossaryDict[firstChar].push(glossaries[i])
+                }
+
+                return glossaryDict
+            },
             getGlossaryList: function () {
                 this.$store.dispatch('mlGlossary/SEARCH_GLOSSARY')
             },
@@ -43,6 +88,9 @@
             },
             glossaries() {
                 return this.$store.state.mlGlossary.glossaryList
+            },
+            alphabetGlossaries(){
+                return this.getAlphabetGlossary()
             }
         }
     }
