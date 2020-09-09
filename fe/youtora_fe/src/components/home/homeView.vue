3d9941d10020353a90169029f7336e977250e45c
DicoTiar <mw18074@bristol.ac.uk>
2020-09-09 19:08:07 +0900

    hotfix: logo and searchBar size update #125

--- fe/youtora_fe/src/components/home/homeView.vue
+++ fe/youtora_fe/src/components/home/homeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="homeView" class="homeView">
         <div class="homeSearchBar">
-            <img :src="require('../../assets/tmp_logo.png')" alt="Youtora" style="width: 600px"/>
+            <img :src="require('../../assets/tmp_logo.png')" alt="Youtora" style="width: 75%"/>
             <home-body/>
         </div>
 
@@ -53,7 +53,7 @@
     /* Not required for template or sticky footer method. */
     .homeSearchBar{
         position: fixed;
-        width: 600px;
+        width: 50%;
         top: 40%;
         left: 50%;
         transform: translate(-50%, -50%);
