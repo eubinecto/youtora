2fb8496adca301955245cb8152713c26020b2f61
DicoTiar <mw18074@bristol.ac.uk>
2020-09-10 10:46:51 +0900

    loading spinner added #125

--- fe/youtora_fe/src/components/generalSearch/generalSearchVideoList.vue
+++ fe/youtora_fe/src/components/generalSearch/generalSearchVideoList.vue
@@ -1,75 +1,81 @@
 <template>
     <div id="generalSearchVideoList">
-        <b-card class="searchResult"
-                v-for="item in videoList"
-                :key="videoList.indexOf(item)">
-            <b-card-group>
-                <b-card style="min-width: 450px">
-                    <b-embed
-                            type="iframe"
-                            aspect="16by9"
-                            :src="urlConverter(item.tracks[1].url)"
-                            allowfullscreen
-                    />
-                </b-card>
-                <b-card style="min-width: 450px">
-                    <b-card-group id="subtitles">
-                        <b-card class="subtitleSection border-white" style="font-size: 150%; vertical-align: center">
-                            <span>..... </span>
-                            <span v-html="item.highlight.context[0]"></span>
-                            <span> .....</span>
-                        </b-card>
-                    </b-card-group>
-                </b-card>
-            </b-card-group>
+<!--        <div class="spinner-grow text-info" role="status" v-if="isLoading === true" style="margin-top: 20%">-->
+<!--            <span class="sr-only">Loading...</span>-->
+<!--        </div>-->
+        <div>
+            <b-card class="searchResult"
+                    v-for="item in videoList"
+                    :key="videoList.indexOf(item)">
+                <b-card-group>
+                    <b-card style="min-width: 450px">
+                        <b-embed
+                                type="iframe"
+                                aspect="16by9"
+                                :src="urlConverter(item.tracks[1].url)"
+                                allowfullscreen
+                        />
+                    </b-card>
+                    <b-card style="min-width: 450px">
+                        <b-card-group id="subtitles">
+                            <b-card class="subtitleSection border-white" style="font-size: 150%; vertical-align: center">
+                                <span>..... </span>
+                                <span v-html="item.highlight.context[0]"></span>
+                                <span> .....</span>
+                            </b-card>
+                        </b-card-group>
+                    </b-card>
+                </b-card-group>
 
-            <b-card-group id="metaInfo" style="margin-bottom: 20px">
-                <b-card style="font-size: 90%">
-                    <b-card-group>
-                        <b-card class="border-white" style="min-width: 140px">
+                <b-card-group id="metaInfo" style="margin-bottom: 20px">
+                    <b-card style="font-size: 90%">
+                        <b-card-group>
+                            <b-card class="border-white" style="min-width: 140px">
                             <span>
                                 <font-awesome-icon icon="thumbs-up" size="lg"/>
                                 /
                                 <font-awesome-icon icon="thumbs-down" size="lg"/>
                                 : {{ Math.round(item.features.caption.video.like_ratio * 10000) /100 }}%
                             </span>
-                        </b-card>
-                        <b-card class="border-white" style="min-width: 140px">
+                            </b-card>
+                            <b-card class="border-white" style="min-width: 140px">
                             <span>
                                 <font-awesome-icon icon="bookmark" size="lg"/>
                                 : {{ item.features.caption.video.channel.subs }}
                             </span>
-                        </b-card>
-                        <b-card class="border-white" style="min-width: 140px">
+                            </b-card>
+                            <b-card class="border-white" style="min-width: 140px">
                             <span>
                                 <font-awesome-icon icon="eye" size="lg"/>
                                 : {{ item.features.caption.video.views }}
                             </span>
-                        </b-card>
-                        <b-card class="border-white" style="min-width: 140px">
+                            </b-card>
+                            <b-card class="border-white" style="min-width: 140px">
                             <span>
                                 <font-awesome-icon icon="closed-captioning" size="lg"/>
                                 : {{ isAuto(item.features.caption.is_auto) }}
                             </span>
-                        </b-card>
-                        <b-card class="border-white" style="min-width: 140px">
+                            </b-card>
+                            <b-card class="border-white" style="min-width: 140px">
                             <span>
                                 <font-awesome-icon icon="language" size="lg"/>
                                 : {{ item.features.caption.lang_code }}
                             </span>
-                        </b-card>
-                        <b-card class="border-white" style="min-width: 150px">
+                            </b-card>
+                            <b-card class="border-white" style="min-width: 150px">
                             <span>
                                 <font-awesome-icon icon="filter" size="lg"/>
                                 : {{ item.features.caption.video.category }}
                             </span>
-                        </b-card>
-                    </b-card-group>
+                            </b-card>
+                        </b-card-group>
 
-                </b-card>
+                    </b-card>
 
-            </b-card-group>
-        </b-card>
+                </b-card-group>
+            </b-card>
+        </div>
+
     </div>
 </template>
 
