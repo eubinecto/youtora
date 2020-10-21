d50bb27c47bc1bfe5b01cafdaf0a1968de82a556
Eu-Bin KIM <tlrndk123@gmail.com>
2020-10-21 15:46:27 +0900

    [#160] added ExtractIdiomsRunner. But having some database integrity issues.

---
+++ youtora/refine/runners.py
@@ -0,0 +1,20 @@
+import logging
+
+from youtora.collect.models import IdiomRaw
+from youtora.refine.extractors import IdiomExtractor
+
+
+class ExtractIdiomsRunner:
+    @classmethod
+    def run(cls):
+        logger = logging.getLogger("run")
+        # get all idiom raws
+        idiom_raw_gen = IdiomRaw.objects.all()
+        idiom_gen = (
+            IdiomExtractor.parse(idiom_raw)
+            for idiom_raw in idiom_raw_gen
+        )
+        # save each idiom
+        for idx, idiom in enumerate(idiom_gen):
+            logger.info("idiom saved: #{}".format(idx + 1))
+            idiom.save()
