8b7360e2911bca3ffe385994a0590212539d6135
Eu-Bin KIM <tlrndk123@gmail.com>
2020-11-08 22:40:55 +0900

    rectified host name settings

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'youtora_db',
-        'HOST': "mongodb://192.168.219.197:27017",  # experiment with the local one
+        'CLIENT': {
+            'host': "mongodb://192.168.219.197:27017"
+        }  # experiment with the local one
         # you may or may need username or password
         # experiment with the localhost first
         # MONGO_URI = "mongodb://localhost:27017"
